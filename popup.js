6db803800ac8f9008d6cb751d8c744346f64818c
tharagai23102007-blip <tharagai23102007@gmail.com>
2026-06-01 20:04:11 +0530

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Time Tracker</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <h2>productivity Analytics</h2>
+
+        <div class="card">
+            <h3>Total Time</h3>
+            <p id="totalTime">0 min</p>
+        </div>
+        <div class="card">
+            <h3>productivity Score</h3>
+            <div id="score">0%</div>
+        </div>
+
+        <div class="card">
+            <h3>Website Usage</h3>
+            <div id="results"></div>
+        </div>
+
+        <script src="popup.js"></script>
+    </body>
+</html>
